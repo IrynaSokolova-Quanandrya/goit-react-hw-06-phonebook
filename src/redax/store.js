b8bef53243627b746bec9fd0929ffb5b2c56b35c
Iryna <irasokolova25@ukr.net>
2021-12-06 23:19:41 +0200

    add storage

--- src/redax/store.js
+++ src/redax/store.js
@@ -1,9 +1,36 @@
-import { combineReducers, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
 import phonebookReducer from "./reduser";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
-const rootReduser = combineReducers({
-  phonebook: phonebookReducer,
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+
+const persistConfig = {
+  key: "contacts",
+  storage,
+};
+
+export const store = configureStore({
+  reducer: persistReducer(persistConfig, phonebookReducer),
+  middleware,
+  devTools: process.env.NODE_ENV === "development",
 });
-const store = createStore(rootReduser, composeWithDevTools());
-export default store;
+
+export const persistor = persistStore(store);
