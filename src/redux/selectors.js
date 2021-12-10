36f8b2b76b6ed244c4451042e969bdeeeef9b896
Iryna <irasokolova25@ukr.net>
2021-12-10 21:06:29 +0200

    delete useless code

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -6,8 +6,6 @@ export const getFilter = (state) => state.contacts.filter;
 export const getVisibleContacts = createSelector(
   [getContacts, getFilter],
   (contacts, filter) => {
-    // const contacts = getContacts(state);
-    // const filter = getFilter(state);
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter((contact) => {
       return contact.name.toLowerCase().includes(normalizedFilter);
